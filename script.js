3bbe66a2ece161bd417e573f2645d0709ffb663a
tawmae <168794128+tawmae@users.noreply.github.com>
2025-02-01 10:22:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 const longString = "VGhpcyBpcyBhIHZlcnkgbG9uZyBiYXNlNjQgZW5jb2RlZCBzdHJpbmcgdGhhdCB3aWxsIGJlIGNvcGllZCB0byBjbGlwYm9hcmQ=";
-function zeigeNachricht() {
-  document.getElementById("nachricht").innerText = "Hallo von JavaScript!";
-}
+function zeigeNachricht() {}
 window.addEventListener("scroll", function() {
   const header = document.querySelector("header");
-  if (window.scrollY > 50) {
-    header.style.background = "#1a1920";
-  } else {
-    header.style.background = "#1e1d22";
-  }
+  header.style.background = window.scrollY > 50 ? "#1a1920" : "#1e1d22";
 });
 document.addEventListener("DOMContentLoaded", function() {
-  document.querySelector(".import-string").innerText = longString.substring(0, 30) + "...";
+  document.querySelector(".import-string").innerText = longString;
   function updateTime() {
     const now = new Date();
-    document.getElementById("current-time").innerText = "Aktuelle Zeit: " + now.toLocaleTimeString();
+    const timeElem = document.getElementById("current-time");
+    if(timeElem) {
+      timeElem.innerText = "Aktuelle Zeit: " + now.toLocaleTimeString();
+    }
   }
   updateTime();
   setInterval(updateTime, 1000);
@@ -33,9 +30,9 @@ function copyImportString() {
   }, 1500);
 }
 function copyCode(elem) {
-  const codeBlock = elem.previousElementSibling;
-  if (codeBlock && codeBlock.querySelector("code")) {
-    const codeText = codeBlock.querySelector("code").innerText;
+  const preElem = elem.parentElement;
+  if (preElem && preElem.querySelector("code")) {
+    const codeText = preElem.querySelector("code").innerText;
     navigator.clipboard.writeText(codeText);
     const notif = document.createElement("div");
     notif.className = "copy-notification";
