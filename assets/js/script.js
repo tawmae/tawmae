0802b6e976efc0d5a7e95ef92c6a6862b531788c
tawmae <168794128+tawmae@users.noreply.github.com>
2025-02-01 18:51:38 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,23 @@
-var slides, indicators, currentIndex=0;
-function showSlide(i){slides[currentIndex].classList.remove("active");indicators[currentIndex].classList.remove("active");slides[i].classList.add("active");indicators[i].classList.add("active");currentIndex=i;}
-function nextSlide(){var i=(currentIndex+1)%slides.length;showSlide(i);}
-function prevSlide(){var i=(currentIndex-1+slides.length)%slides.length;showSlide(i);}
-document.addEventListener("DOMContentLoaded",function(){
-slides=document.querySelectorAll(".rotator a");
-indicators=document.createElement("div");
-document.querySelector(".rotator-indicators").appendChild(indicators);
-indicators.remove();
-indicators=[];
-slides.forEach(function(_,i){
-var d=document.createElement("div");
-d.className="indicator";
-d.addEventListener("click",function(){showSlide(i);});
-document.querySelector(".rotator-indicators").appendChild(d);
-indicators.push(d);
-});
-slides[0].classList.add("active");
-indicators[0].classList.add("active");
+document.addEventListener("DOMContentLoaded", function() {
+  slides = document.querySelectorAll(".rotator a");
+  indicators = document.createElement("div");
+  document.querySelector(".rotator-indicators").appendChild(indicators);
+  indicators.remove();
+  indicators = [];
+
+  slides.forEach(function(_, i) {
+    var d = document.createElement("div");
+    d.className = "indicator";
+    d.addEventListener("click", function() {
+      showSlide(i);
+    });
+    document.querySelector(".rotator-indicators").appendChild(d);
+    indicators.push(d);
+  });
+
+  slides[0].classList.add("active");
+  indicators[0].classList.add("active");
+
+
+  setInterval(nextSlide, 8000);  
 });
