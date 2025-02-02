deaffd17db4bff8a43d4494c7e3d280de666b375
tawmae <168794128+tawmae@users.noreply.github.com>
2025-02-02 13:17:30 +0100

    Update extension-script.js

--- assets/js/extension-script.js
+++ assets/js/extension-script.js
@@ -1,6 +1,7 @@
 function toggleAccordion(id) {
   const content = document.getElementById(id);
-  const icon = content.previousElementSibling.querySelector('.iconify');
+  const icon = content.previousElementSibling.querySelector('.accordion-icon');
+
   if (content.style.display === 'block') {
     content.style.display = 'none';
     icon.setAttribute("data-icon", "ic:baseline-keyboard-arrow-right");
@@ -10,17 +11,18 @@ function toggleAccordion(id) {
   }
 }
 
+
 function copyToClipboard() {
   const copyText = document.getElementById("base64-string");
   const button = copyText.nextElementSibling;
   
-  // Kopiere den Text in die Zwischenablage
+ 
   navigator.clipboard.writeText(copyText.getAttribute("data-copy-text")).then(() => {
-    // Ändere den Button-Text und das Symbol
+   
     button.innerHTML = '<span class="iconify" data-icon="fluent:checkmark-20-filled"></span> Copied';
     button.classList.add('copied');
     
-    // Setze den Button nach 2,5 Sekunden zurück
+   
     setTimeout(() => {
       button.innerHTML = '<span class="iconify" data-icon="material-symbols:content-copy-outline-sharp"></span> Copy';
       button.classList.remove('copied');
