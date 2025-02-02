c6bc9341e09d7878a227ce28d89fa483e7a6bad5
tawmae <168794128+tawmae@users.noreply.github.com>
2025-02-02 20:36:36 +0100

    Update extension-script.js

--- assets/js/extension-script.js
+++ assets/js/extension-script.js
@@ -1,65 +1,64 @@
 // ====================================================================================================================
 function toggleAccordion(id) {
-  const content = document.getElementById(id);
-  const icon = content.previousElementSibling.querySelector('.accordion-icon');
+    const content = document.getElementById(id);
+    const icon = content.previousElementSibling.querySelector('.accordion-icon');
 
-  if (content.style.display === 'block') {
-    content.style.display = 'none';
-    icon.setAttribute("data-icon", "ic:baseline-keyboard-arrow-right");
-  } else {
-    content.style.display = 'block';
-    icon.setAttribute("data-icon", "ic:baseline-keyboard-arrow-down");
-  }
+    if (content.style.display === 'block') {
+        content.style.display = 'none';
+        icon.setAttribute("data-icon", "ic:baseline-keyboard-arrow-right");
+    } else {
+        content.style.display = 'block';
+        icon.setAttribute("data-icon", "ic:baseline-keyboard-arrow-down");
+    }
 }
 
 // ====================================================================================================================
 function copyToClipboard(id) {
-  const copyText = document.getElementById(id);
-  const button = copyText.nextElementSibling;
-  
-  navigator.clipboard.writeText(copyText.getAttribute("data-copy-text")).then(() => {
-    button.innerHTML = '<span class="iconify" data-icon="fluent:checkmark-20-filled"></span> Copied';
-    button.classList.add('copied');
-    
-    setTimeout(() => {
-      button.innerHTML = '<span class="iconify" data-icon="material-symbols:content-copy-outline-sharp"></span> Copy';
-      button.classList.remove('copied');
-    }, 2500);
-  });
+    const copyText = document.getElementById(id);
+    const button = copyText.nextElementSibling;
+
+    navigator.clipboard.writeText(copyText.getAttribute("data-copy-text")).then(() => {
+        button.innerHTML = '<span class="iconify" data-icon="fluent:checkmark-20-filled"></span> Copied';
+        button.classList.add('copied');
+
+        setTimeout(() => {
+            button.innerHTML = '<span class="iconify" data-icon="material-symbols:content-copy-outline-sharp"></span> Copy';
+            button.classList.remove('copied');
+        }, 2500);
+    });
 }
 
 // ====================================================================================================================
 
 function copyURLToClipboard(url) {
 
-  const button = document.querySelector('#import .copy-btn');
-  navigator.clipboard.writeText(url).then(() => {
-    button.innerHTML = '<span class="iconify" data-icon="fluent:checkmark-20-filled"></span> Copied';
-    button.classList.add('copied');
-    setTimeout(() => {
-      button.innerHTML = '<span class="iconify" data-icon="material-symbols:content-copy-outline-sharp"></span> Copy';
-      button.classList.remove('copied');
-    }, 2500);
-  }).catch(err => {
-  });
+    const button = document.querySelector('#import .copy-btn');
+    navigator.clipboard.writeText(url).then(() => {
+        button.innerHTML = '<span class="iconify" data-icon="fluent:checkmark-20-filled"></span> Copied';
+        button.classList.add('copied');
+        setTimeout(() => {
+            button.innerHTML = '<span class="iconify" data-icon="material-symbols:content-copy-outline-sharp"></span> Copy';
+            button.classList.remove('copied');
+        }, 2500);
+    });
 }
 
 // ====================================================================================================================
 function loadImportString(file) {
-  const importText = document.getElementById("import-string");
-  fetch(file)
-    .then(response => response.text())
-    .then(data => {
-    
-      importText.textContent = data.substring(0, 100) + '...'; 
-      importText.setAttribute("data-copy-text", data);
-    })
-    .catch(err => {
-    });
+    const importText = document.getElementById("import-string");
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+
+            importText.textContent = data.substring(0, 100) + '...';
+            importText.setAttribute("data-copy-text", data);
+        })
+        .catch(err => {
+        });
 }
 
 
-document.addEventListener("DOMContentLoaded", function() {
-  loadImportString("/action-imports/dynamic-timers.txt");
+document.addEventListener("DOMContentLoaded", function () {
+    loadImportString("/action-imports/dynamic-timers.txt");
 });
 // ====================================================================================================================
