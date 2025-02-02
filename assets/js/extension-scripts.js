e7d0968451ece69600fdd9401a8ab7d32d3537ba
tawmae <168794128+tawmae@users.noreply.github.com>
2025-02-02 12:01:00 +0100

    Update extension-scripts.js

--- assets/js/extension-scripts.js
+++ assets/js/extension-scripts.js
@@ -9,3 +9,22 @@ function toggleAccordion(id) {
     icon.setAttribute("data-icon", "ic:baseline-keyboard-arrow-down");
   }
 }
+
+function copyToClipboard() {
+  const copyText = document.getElementById("base64-string");
+  const button = copyText.nextElementSibling;
+  
+  // Kopiere den Text in die Zwischenablage
+  navigator.clipboard.writeText(copyText.getAttribute("data-copy-text")).then(() => {
+    // Ändere den Button-Text und das Symbol
+    button.innerHTML = '<span class="iconify" data-icon="fluent:checkmark-20-filled"></span> Copied';
+    button.classList.add('copied');
+    
+    // Setze den Button nach 2,5 Sekunden zurück
+    setTimeout(() => {
+      button.innerHTML = '<span class="iconify" data-icon="material-symbols:content-copy-outline-sharp"></span> Copy';
+      button.classList.remove('copied');
+    }, 2500);
+  });
+}
+
