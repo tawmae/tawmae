7f0b337e1317459c034cf4d65405e8ec970168ab
tawmae <168794128+tawmae@users.noreply.github.com>
2025-02-02 20:19:14 +0100

    Update extension-script.js

--- assets/js/extension-script.js
+++ assets/js/extension-script.js
@@ -13,10 +13,9 @@ function toggleAccordion(id) {
 }
 
 // ====================================================================================================================
-function copyToClipboard(id) {
-  const copyText = document.getElementById(id);
+function copyToClipboard(copyText) {
   const button = copyText.nextElementSibling;
-  
+
   navigator.clipboard.writeText(copyText.getAttribute("data-copy-text")).then(() => {
     button.innerHTML = '<span class="iconify" data-icon="fluent:checkmark-20-filled"></span> Copied';
     button.classList.add('copied');
@@ -27,6 +26,7 @@ function copyToClipboard(id) {
     }, 2500);
   });
 }
+
 
 // ====================================================================================================================
 function loadImportString(file) {
