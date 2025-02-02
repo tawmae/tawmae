a7c54500c69ee9fe82a73fba0a6919d7583c44bf
tawmae <168794128+tawmae@users.noreply.github.com>
2025-02-02 01:10:26 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,11 +40,6 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-
-document.addEventListener("DOMContentLoaded", function() {
-  replaceWordsWithEmojis();
-});
-
 var slides, indicators, currentIndex = 0;
 
 function showSlide(i) {
