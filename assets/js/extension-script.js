e4519397d1465f09b01f1de49a453c288878caf0
tawmae <168794128+tawmae@users.noreply.github.com>
2025-02-02 20:24:21 +0100

    Update extension-script.js

--- assets/js/extension-script.js
+++ assets/js/extension-script.js
@@ -1,50 +1,51 @@
 // ====================================================================================================================
 function toggleAccordion(id) {
-  const content = document.getElementById(id);
-  const icon = content.previousElementSibling.querySelector('.accordion-icon');
-
-  if (content.style.display === 'block') {
-    content.style.display = 'none';
-    icon.setAttribute("data-icon", "ic:baseline-keyboard-arrow-right");
-  } else {
-    content.style.display = 'block';
-    icon.setAttribute("data-icon", "ic:baseline-keyboard-arrow-down");
+    const content = document.getElementById(id);
+    const icon = content.previousElementSibling.querySelector('.accordion-icon');
+  
+    if (content.style.display === 'block') {
+      content.style.display = 'none';
+      icon.setAttribute("data-icon", "ic:baseline-keyboard-arrow-right");
+    } else {
+      content.style.display = 'block';
+      icon.setAttribute("data-icon", "ic:baseline-keyboard-arrow-down");
+    }
   }
-}
-
-// ====================================================================================================================
-function copyToClipboard(copyText) {
-  navigator.clipboard.writeText(copyText).then(() => {
-    const button = event.target;
-    button.innerHTML = '<span class="iconify" data-icon="fluent:checkmark-20-filled"></span> Copied';
-    button.classList.add('copied');
-    
-    setTimeout(() => {
-      button.innerHTML = '<span class="iconify" data-icon="material-symbols:content-copy-outline-sharp"></span> Copy';
-      button.classList.remove('copied');
-    }, 2500);
-  }).catch(err => {
-    console.error('Fehler beim Kopieren: ', err);
-  });
-
-
-// ====================================================================================================================
-function loadImportString(file) {
-  const importText = document.getElementById("import-string");
-  fetch(file)
-    .then(response => response.text())
-    .then(data => {
-    
-      importText.textContent = data.substring(0, 100) + '...'; 
-      importText.setAttribute("data-copy-text", data);
-    })
-    .catch(err => {
+  
+  // ====================================================================================================================
+  function copyToClipboard(copyText) {
+    navigator.clipboard.writeText(copyText).then(() => {
+      const button = event.target;
+      button.innerHTML = '<span class="iconify" data-icon="fluent:checkmark-20-filled"></span> Copied';
+      button.classList.add('copied');
       
+      setTimeout(() => {
+        button.innerHTML = '<span class="iconify" data-icon="material-symbols:content-copy-outline-sharp"></span> Copy';
+        button.classList.remove('copied');
+      }, 2500);
+    }).catch(err => {
+      console.error('Fehler beim Kopieren: ', err);
     });
 }
-
-
-document.addEventListener("DOMContentLoaded", function() {
-  loadImportString("/action-imports/dynamic-timers.txt");
-});
-// ====================================================================================================================
+  
+  
+  // ====================================================================================================================
+  function loadImportString(file) {
+    const importText = document.getElementById("import-string");
+    fetch(file)
+      .then(response => response.text())
+      .then(data => {
+      
+        importText.textContent = data.substring(0, 100) + '...'; 
+        importText.setAttribute("data-copy-text", data);
+      })
+      .catch(err => {
+        
+      });
+  }
+  
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    loadImportString("/action-imports/dynamic-timers.txt");
+  });
+  // ====================================================================================================================
